744dd023f0ec5c95896c5f9f03c915cab5823043
Marc Zahn <marc.zahn@loopline-systems.com>
2016-09-02 00:10:23 +0200

    First implementation with frontend

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,26 +1,94 @@
+function getColorClassesForStatus(status) {
+    if (status == 'STOPPED') {
+        return 'yellow darken-3';
+    }
+    if (status == 'FATAL') {
+        return 'red darken-1';
+    }
+    if (status == 'RUNNING') {
+        return 'green darken-1';
+    }
+
+    return '';
+}
+
 var NodeList = React.createClass({
     getInitialState: function() {
-        return {data: []};
+        return {nodes: []};
     },
     render: function() {
-        return (<div>Node list</div>)
+        return (
+            <div className="node-list">
+                {this.state.nodes.map(function(node, i) {
+                    return <NodeItem node={node}/>
+                })}
+            </div>
+        );
     },
     componentDidMount: function() {
-        $.ajax({
-            url: '/nodes',
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-                this.setState({data: data});
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
+        var self = this;
+        var loadNodes = function() {
+            fetch('/nodes', {
+                method: 'get',
+                cache: false
+            }).then(function(response) {
+                return response.json();
+            }).then(function(nodes) {
+                self.setState({
+                    nodes: nodes
+                });
+            });
+        };
+
+        window.setInterval(loadNodes, 3000);
     }
 });
 
+var NodeItem = React.createClass({
+    render: function() {
+        var node = this.props.node;
+        var stateClass = 'new badge ' + (node.state.name == 'RUNNING' ? 'green' : 'red');
 
+        return (
+            <section class="node">
+                <h4 className="header">{node.name}<span className={stateClass} data-badge-caption={node.state.name}/></h4>
+                <table className="processes responsive-table bordered">
+                    <thead>
+                        <tr>
+                            <th>Process</th>
+                            <th>PID</th>
+                            <th>Up since</th>
+                            <th>Down since</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {node.processes.map(function(process, i) {
+                            var stateClass = getColorClassesForStatus(process.state.name);
+
+                            var started = (new Date(process.start * 1000)).toUTCString();
+                            var stopped = '-';
+                            if (process.stop != 0) {
+                                started = '-';
+                                stopped = (new Date(process.stop * 1000)).toUTCString();
+                            }
+
+                            return (
+                                <tr className={stateClass}>
+                                    <td>{process.name}</td>
+                                    <td>{process.pid || "-"}</td>
+                                    <td>{started}</td>
+                                    <td>{stopped}</td>
+                                    <td>{process.state.name}</td>
+                                </tr>
+                             )
+                         })}
+                    </tbody>
+                </table>
+            </section>
+        );
+    }
+});
 
 ReactDOM.render(
     <NodeList />,
--- webroot/js/components/NodeList.js
+++ webroot/js/components/NodeList.js
@@ -1,4 +1,12 @@
 var NodeList = React.createClass({
+    getInitialState = function() {
+        return [];
+    }
+    componentDidMount = function() {
+        $.get('/nodes', function(result) {
+            console.dir(result);
+        })
+    },
     render: function() {
         return (<div>Node list</div>)
     }
